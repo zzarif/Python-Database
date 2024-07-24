277e6d6304837b5be8781089092996d32a315028
Zibran Zarif Amio <zibran.zarif.amio@gmail.com>
2024-07-24 23:50:43 +0600

    stack update

---
+++ leetcode/stack/generate_parantheses.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generateParenthesis(self, n):
+        
+        return
+    
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.generateParenthesis(3))
