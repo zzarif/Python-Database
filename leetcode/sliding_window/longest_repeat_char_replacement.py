9d0eecc047d4a82dfa1a6808933e9989136b364b
Zibran Zarif Amio <zibran.zarif.amio@gmail.com>
2024-08-02 23:15:49 +0600

    sliding window update

---
+++ leetcode/sliding_window/longest_repeat_char_replacement.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+
+            if (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+
+        return (r - l + 1)
+    
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.characterReplacement(s = "ABAA", k = 0))
